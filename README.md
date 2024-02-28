9d926e46d1af7416063cc95300a144d2d224dade
Niklas <niklas.kinze@gmail.com>
2024-02-28 08:21:27 +0100

    feat: bot only listens to one given channel
    docs: added docs

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-Service File for systemctl: 
+Service file for systemctl: 
 
-Path: /etc/systemd/system/name.service
+Path: /etc/systemd/system/*name*.service
 ```
 [Unit]
-Description=Discord Bot fuer den ARK Server
+Description=*Discord Bot fuer den ARK Server*
 After=multi-user.target
 [Service]
 Type=simple
 Restart=always
-ExecStart=/usr/bin/python3 /etc/ark/arkbot/bot.py
+ExecStart=/usr/bin/python3 */etc/ark/arkbot/bot.py*
 [Install]
 WantedBy=multi-user.target
 ```
+
+1. Put the file there for the service to work properly
+2. Remember to change the path, the name and the description
+3. Use `sudo systemctl daemon-reload` to reload the services
+4. Start and enable with `sudo systemctl start *name*.service` and `sudo systemctl enable *name*.service`
+5. Check status with `sudo systemctl status *name*.service`
--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 # written by niklas k
 import os
-# import subprocess
 import docker
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
+# set token in .env file with attribute DISCORD_TOKEN
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 dockerClient = docker.from_env()
@@ -18,38 +18,41 @@ class MyClient(discord.Client):
         await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="/server help"))
 
     async def on_message(self, message):
+        # the channel the bot answers in
         channel = client.get_channel(1202967389579255919)
-        if (message.content == "/server help"):
-            await channel.send(f'Folgende Commands stehen zur Verfügung: ```\n/server start\n/server stop\n/server status\n/server backup\n```')
-        elif (message.content == "/server start"):
-            await channel.send(f'{message.author.mention} Alles klar, ich geb mein bestes....')
-            if (serverContainer.status == "exited"):
-                serverContainer.start()
-                await channel.send(f'Der Container war gestoppt, also starte ich ihn erstmal neu. Der Server sollte in 3-5 Minuten online sein.')
-            else:
-                res = serverContainer.exec_run("manager start")
-                await channel.send(f'```\n{res[1]}\n```')
-        elif (message.content == "/server stop"):
-            await channel.send(f'{message.author.mention} Mal schauen ob der Bro gestoppt werden kann.')
-            if (serverContainer.status == "exited"):
-                await channel.send(f'Der Server ist bereits gestoppt.')
-            else:
-                res = serverContainer.exec_run("manager stop")
-                await channel.send(f'Der Server wurde gestoppt. ```\n{res[1]}\n```')
-        elif (message.content == "/server status"):
-            await channel.send(f'{message.author.mention} Uno Momento! Ich frag mal ganz lieb nach dem Status.')
-            if (serverContainer.status == "exited"):
-                await channel.send(f'Der Container läuft nicht.')
-            else:
-                res = serverContainer.exec_run("manager status --full")
-                await channel.send(f'```\n{res[1]}\n```')
-        elif (message.content == "/server backup"):
-            await channel.send(f'{message.author.mention} Backup wird erstellt.')
-            if (serverContainer.status == "exited"):
-                await channel.send(f'Der Container läuft nicht.')
-            else:
-                res = serverContainer.exec_run("manager backup")
-                await channel.send(f'```\n{res[1]}\n```')
+        # the channel the bot listens in
+        if message.channel.id == 1202967389579255919:
+            if (message.content == "/server help"):
+                await channel.send(f'Folgende Commands stehen zur Verfügung: ```\n/server start\n/server stop\n/server status\n/server backup\n```')
+            elif (message.content == "/server start"):
+                await channel.send(f'{message.author.mention} Alles klar, ich geb mein bestes....')
+                if (serverContainer.status == "exited"):
+                    serverContainer.start()
+                    await channel.send(f'Der Container war gestoppt, also starte ich ihn erstmal neu. Der Server sollte in 3-5 Minuten online sein.')
+                else:
+                    res = serverContainer.exec_run("manager update")
+                    await channel.send(f'```\nDer Server sollte in ein paar Minuten online sein.\n```')
+            elif (message.content == "/server stop"):
+                await channel.send(f'{message.author.mention} Mal schauen ob der Bro gestoppt werden kann.')
+                if (serverContainer.status == "exited"):
+                    await channel.send(f'Der Server ist bereits gestoppt.')
+                else:
+                    res = serverContainer.exec_run("manager stop")
+                    await channel.send(f'Der Server wurde gestoppt. ```\n{res[1]}\n```')
+            elif (message.content == "/server status"):
+                await channel.send(f'{message.author.mention} Uno Momento! Ich frag mal ganz lieb nach dem Status.')
+                if (serverContainer.status == "exited"):
+                    await channel.send(f'Der Container laeuft nicht.')
+                else:
+                    res = serverContainer.exec_run("manager status --full")
+                    await channel.send(f'```\n{res[1]}\n```')
+            elif (message.content == "/server backup"):
+                await channel.send(f'{message.author.mention} Backup wird erstellt.')
+                if (serverContainer.status == "exited"):
+                    await channel.send(f'Der Container läuft nicht.')
+                else:
+                    res = serverContainer.exec_run("manager backup")
+                    await channel.send(f'```\n{res[1]}\n```')
 
 
 intents = discord.Intents.default()
